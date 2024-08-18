f862fd5055d33d0ed08bffa63ed91f697826d5c5
Lara <lara@Laras-MacBook-Air.local>
2024-08-19 00:19:54 +0200

    implement writing to minio raw

--- consumer/consumer.py
+++ consumer/consumer.py
@@ -1,7 +1,14 @@
+import os
+import abc 
+from datetime import datetime
+from typing import List
+
 import glob
 import json
-import os
-from datetime import datetime
+
+from io import BytesIO
+from minio import Minio
+from minio.error import S3Error
 
 from confluent_kafka import Consumer
 from google.protobuf.json_format import MessageToJson
@@ -16,7 +23,6 @@ CONFIG_FILE_PATH = get_config_path()
 # (1) Create consumer instance
 # ==================================== #
 
-
 def create_kafka_consumer() -> Consumer:
     config_parser = ConfigParser(CONFIG_FILE_PATH)
     kafka_consumer_config_dict = config_parser.get_consumer_config()
@@ -27,11 +33,9 @@ def create_kafka_consumer() -> Consumer:
 
 # TODO: Do we need message received report? (by analogy with message delivery for producer). Can be done with Prometheus
 
-
 # ==================================== #
-# (2) Consume messages from kafka
+# (2) Parse messages from kafka
 # ==================================== #
-
 
 def parse_message(message):
     event_type = ProducerKeyManager(producer_key=message.key().decode("utf-8")).get_event_type_from_key()
@@ -50,17 +54,42 @@ def parse_message(message):
         return event_json_data
 
 
-# TODO: Implement messages "buffer" on consumer-side: store N messages in-memory,
-# and after certain number of messages appear or by the timer (let's say 5 sec.)
+# ==================================== #
+# (3) Set up stuff for Minio
+# ==================================== #
 
+def create_minio_client() -> Minio: 
+    # TODO: Move credentials from here to stg config
+    minio_client = Minio(
+                "localhost:9000", 
+                # following keys are from the environment variables in docker-compose.yaml 
+                access_key="minio_user",  
+                secret_key="minio_password",  
+                secure=False  # Set to True if using HTTPS
+            )
+    return minio_client
 
-class LocalFileWriter:
-    def __init__(self, event_json_data, environment, root_path, max_output_file_size):
+def create_bucket(bucket_name: str, minio_client: Minio) -> None:
+    try:
+        if not minio_client.bucket_exists(bucket_name):
+            minio_client.make_bucket(bucket_name)
+            print(f"Bucket '{bucket_name}' created successfully.")
+        else:
+            print(f"Bucket '{bucket_name}' already exists.")
+    except S3Error as exc:
+        print("Error occurred: ", exc)
+
+# ==================================== #
+# (3) Set up FileWriters
+# ==================================== #
+
+class FileWriter(abc.ABC):
+    # NOTE: This is an abstract class to make file writing env agnostic
+
+    def __init__(self, event_json_data, max_output_file_size):
         self._event_json_data = event_json_data
-        self._environment = environment
-        self._root_path = root_path
         self._max_output_file_size = max_output_file_size
-
+    
     def parse_received_at_date(self):
         received_at_timestamp = datetime.fromtimestamp(int(self._event_json_data["context"]["receivedAt"]))
         date_dict = {
@@ -71,124 +100,245 @@ class LocalFileWriter:
             "int_timestamp": str(self._event_json_data["context"]["receivedAt"]),
         }
         return date_dict
+    
+    def get_or_create_file_path(self, json_files: List[str], folder_path: str, date_dict: dict) -> str:
+        # (1) Check existing files
+        if json_files:
+            # Select "most recent" file (by its event-timestamp name),
+            most_recent_file_path = max(json_files, key=lambda f: int(os.path.splitext(os.path.basename(f))[0]))
+            # Check its size in bytes
+            # This needs to be overridden by subclasses because they handle file sizes differently
+            file_size = self.get_file_size(most_recent_file_path)
 
-    def get_full_path(self):
-        if self._environment == "dev":
-            date_dict = self.parse_received_at_date()
-            # Check if the subfolder exists in the consumer_output folder
-            folder_path = os.path.join(
-                self._root_path, date_dict["year"], date_dict["month"], date_dict["day"], date_dict["hour"]
-            )
-
-            # Create the folder if it doesn't exist
-            if not os.path.exists(folder_path):
-                os.makedirs(folder_path, exist_ok=True)
-
-            # (1) Get all json files in the dir
-            json_files = glob.glob(os.path.join(folder_path, "*.json"))
-
-            # (2) Check existing files
-            if json_files:
-                # Select "most recent" file (by its event-timestamp name),
-                most_recent_file_path = max(json_files, key=lambda f: int(os.path.splitext(os.path.basename(f))[0]))
-                # Check its size in bytes
-                file_size = os.path.getsize(most_recent_file_path)
-
-                # Define the path file will be written to
-                if file_size < self._max_output_file_size:
-                    file_path = most_recent_file_path
-                else:
-                    file_name = f"{date_dict['int_timestamp']}.json"
-                    file_path = os.path.join(folder_path, file_name)
-
-            # (3) Create file if there are no files
-            if not json_files:
+            # Define the path file will be written to
+            if file_size < self._max_output_file_size:
+                file_path = most_recent_file_path
+            else:
                 file_name = f"{date_dict['int_timestamp']}.json"
                 file_path = os.path.join(folder_path, file_name)
 
-            return file_path
+        # (2) Create file if there are no files
+        if not json_files:
+            file_name = f"{date_dict['int_timestamp']}.json"
+            file_path = os.path.join(folder_path, file_name)
 
+        return file_path
+
+    @abc.abstractmethod
+    def get_file_size(self, file_path: str) -> int:
+        pass
+
+    @abc.abstractmethod
+    def get_full_path(self) -> str:
+        pass
+
+    @abc.abstractmethod
+    def write_file(self):
+        pass
+
+
+class LocalFileWriter(FileWriter):
+    def __init__(self, event_json_data, root_path, max_output_file_size):
+        super().__init__(event_json_data, max_output_file_size)
+        self._root_path = root_path
+
+    def get_file_size(self, file_path: str) -> int:
+        return os.path.getsize(file_path)
+
+    def get_full_path(self) -> str:
+        date_dict = self.parse_received_at_date(self._event_json_data)
+
+        # (1) Check if the subfolder exists in the consumer_output folder
+        folder_path = os.path.join(
+            self._root_path, date_dict["year"], date_dict["month"], date_dict["day"], date_dict["hour"]
+        )
+
+        # (2) Create the folder if it doesn't exist
+        if not os.path.exists(folder_path):
+            os.makedirs(folder_path, exist_ok=True)
+
+        # (3) Get all json files in the dir
+        json_files = glob.glob(os.path.join(folder_path, "*.json"))
+
+        # (4) Get file path
+        file_path = self.get_or_create_file_path(json_files, folder_path, date_dict)
+        
+        return file_path
+
+
+    def write_file(self, event_json_data):
+        file_path = self.get_full_path()
+        if file_path is None:
+            return
+
+        # Read existing data if file exists
+        if os.path.exists(file_path):
+            with open(file_path, "r") as json_file:
+                try:
+                    existing_data = json.load(json_file)
+                except json.JSONDecodeError:
+                    existing_data = []
         else:
-            print(f"No path defined for {self._environment} environment")
+            existing_data = []
+
+        # Ensure existing data is a list (assuming the JSON data structure is a list of events)
+        if not isinstance(existing_data, list):
+            print("The existing data is not a list. Cannot append new data.")
+            return
+
+        # Append the new event data to existing data
+        existing_data.append(self._event_json_data)
+
+        # Write back the updated data
+        with open(file_path, "w") as json_file:
+            json.dump(existing_data, json_file)
+        print(f"JSON file saved to: {file_path}")
+        
+
+
+
+class MinioFileWriter(FileWriter):
+    def __init__(self, event_json_data, root_path, max_output_file_size, minio_client):
+        super().__init__(event_json_data, max_output_file_size)
+        self._root_path = root_path # = bucket_name TODO: should we rename it for comprehensibility?
+        self._minio_client = minio_client
+
+    def get_file_size(self, file_path: str) -> int:
+        object_stat = self._minio_client.stat_object(self._root_path, file_path)
+        return object_stat.size
+
+    def get_full_path(self):
+        date_dict = self.parse_received_at_date()
+
+        # (1) Define folder path
+        folder_path = os.path.join(
+            date_dict["year"], date_dict["month"], date_dict["day"], date_dict["hour"]
+        )
+        # (2) Get all json files in the dir
+        objects = self._minio_client.list_objects(self._root_path, prefix=folder_path, recursive=False)
+        json_files = [obj.object_name for obj in objects if obj.object_name.endswith('.json')]
+
+        # (3) Check existing files
+        file_path = self.get_or_create_file_path(json_files, folder_path, date_dict)
+
+        return file_path
 
     def write_file(self):
-        if self._environment == "dev":
-            file_path = self.get_full_path()
-            if file_path is None:
-                return
+        # (1) Get path
+        file_path = self.get_full_path()
 
-            # Read existing data if file exists
-            if os.path.exists(file_path):
-                with open(file_path, "r") as json_file:
-                    try:
-                        existing_data = json.load(json_file)
-                    except json.JSONDecodeError:
-                        existing_data = []
-            else:
+        # (2) Write
+        # (2.1) Read existing data if the file exists
+        try:
+            response = self._minio_client.get_object(self._root_path, file_path)
+            existing_data = json.load(response)
+            response.close()
+            response.release_conn()  # Important to close the response to avoid connection issues
+        except S3Error as exc:
+            if exc.code == 'NoSuchKey':
                 existing_data = []
-
-            # Ensure existing data is a list (assuming the JSON data structure is a list of events)
-            if not isinstance(existing_data, list):
-                print("The existing data is not a list. Cannot append new data.")
-                return
-
-            # Append the new event data to existing data
-            existing_data.append(self._event_json_data)
-
-            # Write back the updated data
-            with open(file_path, "w") as json_file:
-                json.dump(existing_data, json_file)
-            print(f"JSON file for {self._environment} environment saved to: {file_path}")
-        else:
-            print(f"No saving function defined for {self._environment} environment")
-
-
-def basic_consume_loop(consumer, topics, min_commit_count, save_to_path, max_output_file_size):
-    try:
-        consumer.subscribe(topics)
-        msg_count = 0
-        while True:
-            msg = consumer.poll(timeout=1.0)
-            if msg is None:
-                continue
-
-            if msg.error():
-                print("Kafka message error")  # TODO: Implement logging here
             else:
-                message_dict = parse_message(msg)
-                file_writer = LocalFileWriter(
-                    event_json_data=message_dict,
-                    environment="dev",
-                    root_path=save_to_path,
-                    max_output_file_size=max_output_file_size,
-                )
-                file_writer.write_file()
+                raise  # Re-raise any other exceptions
+    
+        # Ensure existing data is a list (assuming the JSON data structure is a list of events)
+        if not isinstance(existing_data, list):
+            print("The existing data is not a list. Cannot append new data.")
+            return
+        
+        # Append the new event data to existing data
+        existing_data.append(self._event_json_data)
 
-                msg_count += 1
-                if msg_count % min_commit_count == 0:
-                    consumer.commit(asynchronous=True)
-    finally:
-        # Close down consumer to commit final offsets.
-        consumer.close()
+        # Convert updated data back to JSON string
+        json_data = json.dumps(existing_data)
+
+        # Write the updated data back to MinIO
+        self._minio_client.put_object(
+            bucket_name=self._root_path,
+            object_name=file_path,
+            data=BytesIO(json_data.encode("utf-8")),
+            length=len(json_data),
+            content_type="application/json"
+        )
+
+        print(f"JSON file saved to MinIO at: {file_path}")
 
 
 # ==================================== #
-# (3) Test that it works
+# (4) Set up EventConsumer
 # ==================================== #
+
+class EventConsumer:
+    def __init__(self, file_writer: FileWriter, consumer: Consumer, topics: List[str], min_commit_count: int):
+        self._file_writer = file_writer
+        self._consumer = consumer
+        self._topics = topics
+        self._min_commit_count = min_commit_count
+        
+
+    def run_consume_loop(self):
+        try:
+            self._consumer.subscribe(self._topics)
+            msg_count = 0
+            while True:
+                msg = self._consumer.poll(timeout=1.0)
+                if msg is None:
+                    continue
+
+                if msg.error():
+                    print("Kafka message error")  # TODO: Implement logging here
+                else:
+                    message_dict = parse_message(msg)
+                    print("\n>>>>message_dict: ", message_dict)
+                    self._file_writer._event_json_data = message_dict
+                    self._file_writer.write_file()
+                
+                    msg_count += 1
+                    if msg_count % self._min_commit_count == 0:
+                        self._consumer.commit(asynchronous=True)
+        
+        finally:
+            # Close down consumer to commit final offsets.
+            self._consumer.close()
+
+
+
+# ==================================== #
+# (5) Test that it works
+# ==================================== #
+
 
 if __name__ == "__main__":
     # (1) Get general properties
     config_parser = ConfigParser(CONFIG_FILE_PATH)
     general_config_dict = config_parser.get_general_config()
-    # print("\nGeneral config dict:", general_config_dict)
 
-    # (2) Launch consumer
-    consumer = create_kafka_consumer()
-    # print("\nConsumer:", consumer)
-    basic_consume_loop(
-        consumer,
-        [general_config_dict["kafka_topic"]],
-        general_config_dict["min_commit_count"],
-        general_config_dict["save_to_path"],
-        general_config_dict["max_output_file_size"],
+    # (2 Option 1) Instantiate Locacl file writer
+    # file_writer = LocalFileWriter(
+    #     event_json_data={},
+    #     root_path=general_config_dict["root_path"],
+    #     max_output_file_size=general_config_dict["max_output_file_size"]
+    # )
+
+    # (2 Option 2) Instantiate Minio file writer
+    minio_client = create_minio_client()
+    create_bucket(general_config_dict["root_path"], minio_client)
+    file_writer = MinioFileWriter(
+        event_json_data={},
+        root_path=general_config_dict["root_path"],
+        minio_client=minio_client,
+        max_output_file_size=general_config_dict["max_output_file_size"]
     )
+
+    # (3) Instantiate consumer
+    consumer = create_kafka_consumer()
+    event_consumer = EventConsumer(
+        file_writer=file_writer,
+        consumer=consumer,
+        topics=[general_config_dict["kafka_topic"]],
+        min_commit_count=general_config_dict["min_commit_count"]
+    )
+
+    # (4) Write
+    event_consumer.run_consume_loop()
+
+
--- tests/consumer/test_consumer.py
+++ tests/consumer/test_consumer.py
@@ -3,9 +3,54 @@ import threading
 import time
 import uuid
 
-from consumer.consumer import LocalFileWriter, basic_consume_loop, get_config_path
+# from consumer.consumer import LocalFileWriter, basic_consume_loop, get_config_path
+
+from consumer.consumer import LocalFileWriter, EventConsumer, get_config_path
 
 CONFIG_FILE_PATH = get_config_path()
+
+
+# def test_consumption(event_mock, kafka_consumer_mock, clean_up_temp):
+#     # (1) Mock config and define temp folder
+#     general_config_dict = {
+#         "kafka_topic": "event-messages",
+#         "min_commit_count": 10,
+#         "save_to_path": "/tmp",
+#         "max_output_file_size": 1000,
+#     }
+#     temp_folder_uuid = "consumer_test_" + str(uuid.uuid4())
+
+#     # (2) Run consume loop
+#     # test_consumer.py runs in a main thread.
+#     # We make basic_consume_loop to run in a separate thread to be able to end it
+#     # when main testing thread ends (we define it as a daemon, so it is possible)
+#     # Otherwise we enter into an infinite loop and test will never pass
+#     consumer_thread = threading.Thread(
+#         target=basic_consume_loop,
+#         args=(
+#             kafka_consumer_mock,
+#             [general_config_dict["kafka_topic"]],
+#             general_config_dict["min_commit_count"],
+#             general_config_dict["save_to_path"] + f"/{temp_folder_uuid}",
+#             general_config_dict["max_output_file_size"],
+#         ),
+#     )
+#     consumer_thread.daemon = True
+#     consumer_thread.start()
+#     time.sleep(1.5)
+
+#     # (3) Check that file has been created
+#     message_dict = event_mock["event_json_data"]
+#     file_writer = LocalFileWriter(
+#         event_json_data=message_dict,
+#         environment="dev",
+#         root_path=general_config_dict["save_to_path"] + f"/{temp_folder_uuid}",
+#         max_output_file_size=general_config_dict["max_output_file_size"],
+#     )
+
+#     full_file_path = file_writer.get_full_path()
+#     print("full_file_path:", full_file_path)
+#     assert os.path.exists(full_file_path)
 
 
 def test_consumption(event_mock, kafka_consumer_mock, clean_up_temp):
@@ -13,10 +58,27 @@ def test_consumption(event_mock, kafka_consumer_mock, clean_up_temp):
     general_config_dict = {
         "kafka_topic": "event-messages",
         "min_commit_count": 10,
-        "save_to_path": "/tmp",
+        "root_path": "/tmp",
         "max_output_file_size": 1000,
     }
     temp_folder_uuid = "consumer_test_" + str(uuid.uuid4())
+
+    # (2) Instantiate file writer
+    file_writer = LocalFileWriter(
+        event_json_data={},
+        root_path=general_config_dict["root_path"],
+        max_output_file_size=general_config_dict["max_output_file_size"]
+    )
+
+    # (3) Instantiate event consumer
+    event_consumer = EventConsumer(
+        file_writer=file_writer,
+        consumer=kafka_consumer_mock,
+        topics=[general_config_dict["kafka_topic"]],
+        min_commit_count=general_config_dict["min_commit_count"]
+    )
+    # TODO: Mock event consumer / file writer with _event_json_data?
+    print("\n+++++ ", event_consumer._file_writer._event_json_data)
 
     # (2) Run consume loop
     # test_consumer.py runs in a main thread.
@@ -24,14 +86,7 @@ def test_consumption(event_mock, kafka_consumer_mock, clean_up_temp):
     # when main testing thread ends (we define it as a daemon, so it is possible)
     # Otherwise we enter into an infinite loop and test will never pass
     consumer_thread = threading.Thread(
-        target=basic_consume_loop,
-        args=(
-            kafka_consumer_mock,
-            [general_config_dict["kafka_topic"]],
-            general_config_dict["min_commit_count"],
-            general_config_dict["save_to_path"] + f"/{temp_folder_uuid}",
-            general_config_dict["max_output_file_size"],
-        ),
+        target=event_consumer.run_consume_loop(),
     )
     consumer_thread.daemon = True
     consumer_thread.start()
@@ -41,7 +96,6 @@ def test_consumption(event_mock, kafka_consumer_mock, clean_up_temp):
     message_dict = event_mock["event_json_data"]
     file_writer = LocalFileWriter(
         event_json_data=message_dict,
-        environment="dev",
         root_path=general_config_dict["save_to_path"] + f"/{temp_folder_uuid}",
         max_output_file_size=general_config_dict["max_output_file_size"],
     )
